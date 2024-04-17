0cafe5ec18d126eed9c4777cce0f2906a9c145ea
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2024-04-16 20:51:48 -0600

    Initial Commit

---
+++ Project-Organizer-UIKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project-Organizer-UIKit
+//
+//  Created by Alejandro Hernandez on 16/04/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
